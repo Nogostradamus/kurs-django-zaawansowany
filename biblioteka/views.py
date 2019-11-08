a9030a9f6a2eff15d1e7e37652da97223183cb2b
krystian <krystian.czekalski@resourcekraft.com>
2019-11-08 20:54:47 +0000

    Transactions

--- biblioteka/views.py
+++ biblioteka/views.py
@@ -1,28 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # from .signals import nasz_sygnal
-from .models import Autor
-from django.db import IntegrityError, transaction
-from .models import Ksiazka
+from .models import Ksiazka, Autor
+from django.db import transaction
+from django.core.mail import send_mail
 
 def glowna(request):
     # nasz_sygnal.send(sender=Autor, imie="Krystian")
-    autor_id = 1
-    jakias_funkcja(autor_id)
+    autor = {'imie': "Walter", 'nazwisko': 'White'}
+    ksiazka = {'tytul': "Niebieskie cuda", 'rok_wydanie': 2017}
+    dodaj_do_bazy(autor, ksiazka)
     return HttpResponse("To jest nasza glowna strona")
 
 
+def dodaj_do_bazy(autor, ksiazka):
 
-#@transaction.atomic
-def jakias_funkcja(autor_id):
-
-    autor = Autor.objects.get(id=autor_id)
-    ostatnia = Ksiazka.objects.create(tytul="nowy", rok_wydania=2019, autor=autor)
-
-    # try:
-    #     with transaction.atomic():
-    #         generate_relationships()
-    # except IntegrityError:
-    #     handle_exception()
-    #
-    # add_children()
+    with transaction.atomic():
+        nowy_autor = Autor.objects.create(**autor)
+        nowa_ksiazka = Ksiazka(**ksiazka)
+        nowa_ksiazka.autor = nowy_autor
+        nowa_ksiazka.save()
--- zaawansowany/settings.py
+++ zaawansowany/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 
